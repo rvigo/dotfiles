5ee38806f4f23e1a1f07e84407202e215da09cbc
Rafael Vigo <rvigo07+github@gmail.com>
2024-07-04 22:40:53 -0300

    chore: fix typo

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Building gitstatus binary"
-GITSTATUS_DIR=$HOME/.dotfiles/zsh/plugins/git/gistatus
+GITSTATUS_DIR=$HOME/.dotfiles/zsh/plugins/git/gitstatus
 
 cargo build --manifest-path $GITSTATUS_DIR/Cargo.toml --release
 cp $GITSTATUS_DIR/target/release/gitstatus $HOME/.dotfiles/zsh/plugins/git/.
