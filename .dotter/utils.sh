a9442534d17711d39ecbb4b40be7adb315e921cc
Rafael Vigo <rvigo07+github@gmail.com>
2024-07-04 23:12:32 -0300

    chore: changes script location

--- .dotter/utils.sh
+++
@@ -1,7 +0,0 @@
-command_exists() {
-    if command -v "$1" >/dev/null 2>&1; then
-        return 0  # Command exists
-    else
-        return 1  # Command does not exist
-    fi
-}
--- install.sh
+++ install.sh
@@ -3,7 +3,13 @@
 
 set -e
 
-source ./.dotter/utils.sh
+command_exists() {
+    if command -v "$1" >/dev/null 2>&1; then
+        return 0  # Command exists
+    else
+        return 1  # Command does not exist
+    fi
+}
 
 DOTFILES_DIR="$HOME/.dotfiles"
 DOTTER_CONFIG_FOLDER="$DOTFILES_DIR/.dotter/include"
