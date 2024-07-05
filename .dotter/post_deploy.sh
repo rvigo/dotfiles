291634ae35110d08724129e0b34ab608cf0b00ec
Rafael Vigo <rvigo07+github@gmail.com>
2024-07-04 22:59:24 -0300

    chore: includes temp debug commands

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -5,6 +5,14 @@ set -e
 echo "Building gitstatus binary"
 GITSTATUS_DIR=$HOME/.dotfiles/zsh/plugins/git/gitstatus
 
+echo "Checking dirs"
+if [ ! -d $GITSTATUS_DIR ]; then
+    echo "Directory $GITSTATUS_DIR does not exist"
+    exit 1
+fi
+ls $GITSTATUS_DIR
+echo ++++   
+
 cargo build --manifest-path $GITSTATUS_DIR/Cargo.toml --release
 cp $GITSTATUS_DIR/target/release/gitstatus $HOME/.dotfiles/zsh/plugins/git/_gitstatus
 
